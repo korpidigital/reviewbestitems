12b3acf624953543fbdaaf6d087b87c8c88d6021
laturitukku <tuomas.av.korpi@gmail.com>
2022-05-25 18:44:20 +0300

    asd

--- src/routes/Shopcard.svelte
+++ src/routes/Shopcard.svelte
@@ -1,22 +1,29 @@
 <script>
- import {shops} from  "./shopinfo";
+import {shops} from  "./shopinfo";
+
+ function handleClick() {
+		alert('no more alerts')
+	}
 </script>
 
 
 
 {#each  shops as shops, i}
     <div class="card">
-        <div class="name">
-            {shops.name}
+        <div class="pic">
+            <img src={shops.pic} alt="Italian Trulli">
+            
         </div>
         <div class="desc">
             {shops.desc}
         </div>
-        <div class="pic">
-            {shops.pic}
-        </div>
+
         <div class="link">
-            {shops.link}
+ 
+            <button on:click|once={handleClick}>
+                Go to review
+            </button>
+            
         </div>
 
     </div>
@@ -25,7 +32,25 @@
 <style>
 .card{
     flex-direction: row;
-    height: 100px;
+    height: 150px;
     width: 100%;
+    display: flex;
+    padding: 10px;
+}
+
+img {
+ 
+  height: 150px;
+  object-fit: contain;
+}
+.pic{
+ width: 20%;
+ text-align: center;
+}
+.desc{
+    width: 60%;
+}
+.link{
+
 }
 </style>
